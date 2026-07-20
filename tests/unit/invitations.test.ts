b393343a8e5f7838ebfe311b65ebd7adcf3f29ed
GiovB05 <giovannibuontempo05@gmail.com>
2026-07-20 16:30:42 +0200

    Test one-hour invitation contract

--- tests/unit/invitations.test.ts
+++ tests/unit/invitations.test.ts
@@ -9,12 +9,14 @@ import {
   publicAppOrigin,
 } from "../../src/lib/auth/invitations.ts";
 
-test("invitation lifetime defaults to one day", () => {
+test("invitation lifetime defaults to one hour", () => {
   assert.equal(invitationTtlSeconds(undefined), DEFAULT_INVITATION_TTL_SECONDS);
+  assert.equal(DEFAULT_INVITATION_TTL_SECONDS, 3_600);
 });
 
-test("invitation lifetime cannot exceed one day", () => {
+test("invitation lifetime cannot exceed one hour", () => {
   assert.equal(invitationTtlSeconds("604800"), MAX_INVITATION_TTL_SECONDS);
+  assert.equal(MAX_INVITATION_TTL_SECONDS, 3_600);
 });
 
 test("invitation lifetime cannot be shorter than five minutes", () => {
