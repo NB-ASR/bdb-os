7c0f6c20a45353f0938c6e21e5c1bd4ebb979aba
adminbdbos-a11y <admin.bdbos@gmail.com>
2026-07-31 14:18:03 +0100

    Show Accounts Payable status in Purchasing

--- src/app/documents/purchasing/page.tsx
+++ src/app/documents/purchasing/page.tsx
@@ -1,3 +1,4 @@
+import PurchasingAccountsStatus from "./purchasing-accounts-status";
 import PurchasingInventoryStatus from "./purchasing-inventory-status";
 import PurchasingWorkspace from "./purchasing-workspace";
 import styles from "./page.module.css";
@@ -6,6 +7,7 @@ export default function PurchasingPage() {
   return (
     <div className={styles.page}>
       <PurchasingInventoryStatus />
+      <PurchasingAccountsStatus />
       <PurchasingWorkspace />
     </div>
   );
